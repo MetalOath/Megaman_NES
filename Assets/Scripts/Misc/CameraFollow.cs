11f0cbf746a62b6544a203ee2402f5e6aa48455c
OthmanArnaout <arnaout.othman@gmail.com>
2022-06-18 01:35:51 -0400

    L9

--- Assets/Scripts/Misc/CameraFollow.cs
+++ Assets/Scripts/Misc/CameraFollow.cs
@@ -4,26 +4,19 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform player;
-
-    public float minXClamp = 0.1f;
-    public float maxXClamp = 161.27f;
-    public float minYClamp = -0.7f;
-    public float maxYClamp = 26.7f;
+    public float minXClamp = -2.08f;
+    public float maxXClamp = 180.7f;
 
     void LateUpdate()
     {
-        if (player)
+        if (GameManager.instance.playerInstance)
         {
             Vector3 cameraTransform;
 
             cameraTransform = transform.position;
 
-            cameraTransform.x = player.transform.position.x;
+            cameraTransform.x = GameManager.instance.playerInstance.gameObject.transform.position.x;
             cameraTransform.x = Mathf.Clamp(cameraTransform.x, minXClamp, maxXClamp);
-
-            cameraTransform.y = player.transform.position.y;
-            cameraTransform.y = Mathf.Clamp(cameraTransform.y, minYClamp, maxYClamp);
 
             transform.position = cameraTransform;
         }
